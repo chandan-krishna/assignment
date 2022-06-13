c18cc8f071e26e4e33f87d9a7568b13666c6e962
Prashant Parmar <prashant.parmar@programmers.io>
2022-06-13 14:53:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import UserCard from "./components/UserCard";
+import Pagination from "./components/Pagination";
 
-function App() {
+const App = () => {
+  const [selectedUser, setSelectedUser] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path={["/:userId", "/"]} exact>
+          <UserCard
+            selectedUser={selectedUser}
+            setSelectedUser={setSelectedUser}
+          />
+        </Route>
+      </Switch>
+      <Pagination setSelectedUser={setSelectedUser} />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/UserCard.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Card, Icon, Image, Grid, Header, Segment } from "semantic-ui-react";
+import Loader from "./Loader";
+
+const UserCard = ({ selectedUser, setSelectedUser }) => {
+  const { userId } = useParams();
+  const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { first_name, last_name, email, avatar } = user;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setLoading(true);
+      try {
+        const { data: response } = await axios.get(
+          `https://reqres.in/api/users/${selectedUser || userId}`
+        );
+        setUser(response.data);
+      } catch (error) {
+        console.error(error.message);
+        setUser({});
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUser();
+
+    return () => setUser({});
+  }, [userId, selectedUser, setSelectedUser]);
+
+  return (
+    <Segment>
+      <Grid textAlign="center" columns={3}>
+        <Grid.Row>
+          {userId ? (
+            loading ? (
+              <Loader />
+            ) : (
+              <Card>
+                <Image src={avatar} wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>User Details</Card.Header>
+                  <Card.Description>
+                    <p>
+                      <b>First Name: </b> {first_name}
+                    </p>
+                    <p>
+                      <b>Last Name: </b> {last_name}
+                    </p>
+                    <p>
+                      <b>Email: </b> {email}
+                    </p>
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            )
+          ) : (
+            <Segment placeholder>
+              <Header icon>
+                <Icon name="user" />
+                User is not selected. Please click on any button below.
+              </Header>
+            </Segment>
+          )}
+        </Grid.Row>
+      </Grid>
+    </Segment>
+  );
+};
+
+export default UserCard;
